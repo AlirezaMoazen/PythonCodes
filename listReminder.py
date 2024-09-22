e6fb4c1eb4540900d5a0303495fd0c53d0dd5e8f
Alireza Moazen <alireza.moazzenjahromifard@tuhh.de>
2024-09-22 20:46:05 +0200

    first

---
+++ listReminder.py
@@ -0,0 +1,19 @@
+number = int(input("add numbers"))
+numbers =[]
+for i in range(1, number+1):
+    coworkers = {"Name" : input("Name"), "familyName" : input("familyName"), "age" : int(input(age))}
+    numbers.append(coworkers)
+print(coworkers) 
+number=int(input("add number of coworkers"))
+birthday=[]
+for i in range(1, number+1):
+    birthday.append(365*int(input("how old is he?")))
+print (birthday)
+
+print([365*i for i in range(1, int(input("add ur number here: "))+1)2])
+
+number=int(input("add ur nunber here: "))
+numbers=[]
+for i in range(1,number+1):
+    numbers.append(i)
+print(numbers)
