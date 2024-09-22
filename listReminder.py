b1629d8f9182bff7342a5dc867d65af2ed7c0e92
Alireza Moazen <alireza.moazzenjahromifard@tuhh.de>
2024-09-22 20:55:08 +0200

    correction

--- listReminder.py
+++ listReminder.py
@@ -1,19 +1,33 @@
-number = int(input("add numbers"))
-numbers =[]
-for i in range(1, number+1):
-    coworkers = {"Name" : input("Name"), "familyName" : input("familyName"), "age" : int(input(age))}
+number = int(input("Add the number of coworkers: "))
+numbers = []
+
+# Loop for getting coworkers' information
+for i in range(1, number + 1):
+    coworkers = {
+        "Name": input("Name: "),
+        "familyName": input("Family Name: "),
+        "age": int(input("Age: "))
+    }
     numbers.append(coworkers)
-print(coworkers) 
-number=int(input("add number of coworkers"))
-birthday=[]
-for i in range(1, number+1):
-    birthday.append(365*int(input("how old is he?")))
-print (birthday)
 
-print([365*i for i in range(1, int(input("add ur number here: "))+1)2])
+# Print the list of coworkers
+print(numbers)
 
-number=int(input("add ur nunber here: "))
-numbers=[]
-for i in range(1,number+1):
-    numbers.append(i)
+# Loop for calculating coworkers' ages in days
+number_of_coworkers = int(input("How many coworkers for the birthday list? "))
+birthday = []
+for i in range(1, number_of_coworkers + 1):
+    age_in_years = int(input(f"How old is coworker {i}? "))
+    birthday.append(365 * age_in_years)
+
+# Print the list of ages in days
+print(birthday)
+
+# Generate and print a list of numbers multiplied by 365
+number = int(input("Add your number here: "))
+print([365 * i for i in range(1, number + 1)])
+
+# Generate and print a list of numbers from 1 to `number`
+number = int(input("Add your number here: "))
+numbers = [i for i in range(1, number + 1)]
 print(numbers)
